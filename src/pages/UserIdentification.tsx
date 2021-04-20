4d81e64ee6584b0804a1a0d44096c19ee9dac50c
Bruno Marques Gurgel <bmg1612@gmail.com>
2021-04-20 19:23:50 -0300

    fix: Added option to dismiss keyboard

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -8,6 +8,8 @@ import {
 	TextInput,
 	KeyboardAvoidingView,
 	Platform,
+	TouchableWithoutFeedback,
+	Keyboard,
 } from "react-native";
 
 import { Button } from "../components/Button";
@@ -46,28 +48,30 @@ export function UserIdentification() {
 				style={styles.container}
 				behavior={Platform.OS === "ios" ? "padding" : "height"}
 			>
-				<View style={styles.content}>
-					<View style={styles.form}>
-						<View style={styles.header}>
-							<Text style={styles.emoji}>{isFilled ? "😄" : "😀"}</Text>
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+					<View style={styles.content}>
+						<View style={styles.form}>
+							<View style={styles.header}>
+								<Text style={styles.emoji}>{isFilled ? "😄" : "😀"}</Text>
 
-							<Text style={styles.title}>Como podemos {"\n"}chamar você</Text>
-						</View>
-						<TextInput
-							style={[
-								styles.input,
-								(isFocused || isFilled) && { borderColor: colors.green },
-							]}
-							placeholder="Digite um nome"
-							onBlur={handleInputBlur}
-							onFocus={handleInputFocus}
-							onChangeText={handleInputChange}
-						/>
-						<View style={styles.footer}>
-							<Button title="Confirmar" onPress={handleSubmit} />
+								<Text style={styles.title}>Como podemos {"\n"}chamar você</Text>
+							</View>
+							<TextInput
+								style={[
+									styles.input,
+									(isFocused || isFilled) && { borderColor: colors.green },
+								]}
+								placeholder="Digite um nome"
+								onBlur={handleInputBlur}
+								onFocus={handleInputFocus}
+								onChangeText={handleInputChange}
+							/>
+							<View style={styles.footer}>
+								<Button title="Confirmar" onPress={handleSubmit} />
+							</View>
 						</View>
 					</View>
-				</View>
+				</TouchableWithoutFeedback>
 			</KeyboardAvoidingView>
 		</SafeAreaView>
 	);
